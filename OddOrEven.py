8153696bf4220205cd0660c88232e2bcdd842d82
williechow97 <willie.chow01@gmail.com>
2017-12-20 22:49:39 -0800

    Adding py file
    py file containing code of program

---
+++ OddOrEven.py
@@ -0,0 +1,8 @@
+# Program that ask the user for a number. 
+# Depending on whether the number is even or odd, 
+# print out an appropriate message to the user
+
+def main():
+    userNum = input("Please enter a number: ") 
+    
+main()
