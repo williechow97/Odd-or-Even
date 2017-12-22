4d12d97f24a9912f26af49f3701a7ce6f4339101
williechow97 <willie.chow01@gmail.com>
2017-12-22 14:32:58 -0800

    Updated the code to actually do something
    added try except block

--- OddOrEven.py
+++ OddOrEven.py
@@ -3,6 +3,19 @@
 # print out an appropriate message to the user
 
 def main():
-    userNum = input("Please enter a number: ") 
+    try:
+        userNum = int(input("Please enter a number: ")) # get user's input
+                            # catch if user input is incorrect
+        print("The number is odd" if userNum % 2 else "The number of even") # print out correct statement 
+        print('Number is multiple of 4' if userNum % 4 == 0 else '\n') # determine if user's number is mutiple of 4...display message accordingly. 
+        print("Enter two number and I will determine if they divide evenly into each other.")
+        # get two integers from the user
+        num1 = int(input("Enter the numerator: "))
+        num2 = int(input("Enter the denominator: "))
+        print(str(num1),'divdes evenly into', str(num2) if num1 % num2 == 0 else 'There is a remainder') 
+    except TypeError as e:
+        print(str(e))
+    except ZeroDivisionError as e:
+        print(str(e))
     
 main()
